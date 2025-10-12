0b88144bc6358f2c8ca93f5d624046c7b9890aee
Roope Merikukka <roope.merikukka@gmail.com>
2025-10-12 14:13:02 +0300

    Update prod build.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,7 @@ module.exports = merge(common, {
       patterns: [
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
-        { from: 'icon.svg', to: 'icon.svg' },
+        { from: 'fonts', to: 'fonts' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
